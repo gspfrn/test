8f34297c147f4293e43c3ded629b325a248ddc3f
gspfrn <61975305+gspfrn@users.noreply.github.com>
2023-01-04 16:16:54 +0100

    abc

---
+++ locale.py
@@ -0,0 +1,7 @@
+from sympy import *
+x=Symbol('x')
+print integrate(x*sin(x)**5, x)
+# from sympy import *
+# a,b,c,d,e,f,x=symbols('a b c d e f x')
+# print(cancel((a**3-b**3)/(a-b)))
+# print(solve(x**4+b*x**3+c*x**2+d*x+f,x))
